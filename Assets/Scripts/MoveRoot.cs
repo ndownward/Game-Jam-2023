1c89373d13fc0177767a77fbac058aaa22ccdbec
Nicola Downward <nicoladownward@dal.ca>
2023-02-04 11:36:41 -0400

    script changes

--- Assets/Scripts/MoveRoot.cs
+++ Assets/Scripts/MoveRoot.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class MoveRoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
 
-    // Update is called once per frame
-    void Update() {
+    private bool isMouseDragging;
+
+    // Start is called before the first frame update
+    void onMouseDown() {
+            isMouseDragging = true;
+        }
+
+        void onMouseUp() {
+            isMouseDragging = false;
+        }
+
+        // Update is called once per frame
+        void Update() {
+
         // roots respond to user click and drag
+        if (isMouseDragging) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
 
             // make sure roots are always touching
 
         // check if overlapping
+
+
+
+
+        // no overlap? level complete!
 
         
     }
